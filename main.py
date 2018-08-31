e9155999ba3aa1aec57ce02a9d327bf4cd255b34
TunaButter <yunwei.li@carleton.ca>
2018-08-30 20:25:03 -0400

    resize traning frame

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ while(True):
     screen = ImageGrab.grab(bbox=(0, 40, 800, 600))
     screen_np = np.array(screen)
     screen_np_gray = cv2.cvtColor(screen_np, cv2.COLOR_BGR2GRAY)
+    screen_np_gray = cv2.resize(screen_np_gray, (80, 64))
     training_data.append(screen_np_gray)
     cv2.imshow('image', screen_np_gray)
     
