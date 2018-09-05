016d4ab97e41fd4b6fd8701a9a13cbe94d5eee3e
TunaButter <yunwei.li@carleton.ca>
2018-09-04 21:37:45 -0400

    coordinates adjusted

--- process_training_data.py
+++ process_training_data.py
@@ -3,18 +3,19 @@ import cv2
 import utils
 filename = 'data/test_data.npy'
 
-def region_of_interest(img):
-    return
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    mask = cv2.bitwise_and(mask, img)
+    return mask
 
 training_data = np.load(filename)
 
 for frame in training_data:
     frame = cv2.resize(frame, (800, 640))
     frame = cv2.Canny(frame, 40, 80) # TODO: learn; tweak min and max
-    cv2.line(frame, (230,220), (570,220), 255, 5)
-    cv2.line(frame, (0,640),   (800,640), 255, 5)
-    cv2.line(frame, (0,640),   (0,440), 255, 5)
-    cv2.line(frame, (800,640), (800,440), 255, 5)
+    vertices = np.array([[0,640], [800,640], [800,300], [570,200], [230,200], [0,300]])
+    frame = region_of_interest(frame, [vertices])
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
