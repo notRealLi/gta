06bb26a0a95d57e3a8d1fdefc68dfce4b400b0de
TunaButter <yunwei.li@carleton.ca>
2018-09-04 20:37:37 -0400

    roi coordinates

--- process_training_data.py
+++ process_training_data.py
@@ -3,11 +3,18 @@ import cv2
 import utils
 filename = 'data/test_data.npy'
 
+def region_of_interest(img):
+    return
+
 training_data = np.load(filename)
 
 for frame in training_data:
     frame = cv2.resize(frame, (800, 640))
     frame = cv2.Canny(frame, 40, 80) # TODO: learn; tweak min and max
+    cv2.line(frame, (230,220), (570,220), 255, 5)
+    cv2.line(frame, (0,640),   (800,640), 255, 5)
+    cv2.line(frame, (0,640),   (0,440), 255, 5)
+    cv2.line(frame, (800,640), (800,440), 255, 5)
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
